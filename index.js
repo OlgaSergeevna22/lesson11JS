787039f83753530d83909684706c2e2f945de5be
Olga Geres <olgageres13@gmail.com>
2023-03-02 18:07:17 +0300

    change Task3

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
 // // Задание 1
 // // Задать массив имен. Создать 2 переменные, которые будут хранить первых 2 имени, вывести их в консоль. Создать массив, который будет хранить остальные имена, вывести имена из него по одному в консоль. Если кол-во элементов массива меньше 2, то вторая переменная должна стать равна "alex".
 
-const arrNames = ['Olga', 'Ivan', 'Kirill', 'Vasya', 'Alexander', 'Anna', 'Polina']
-    // const arrNames = ['Olga', 'Ivan', 'Kirill'];
+// const arrNames = ['Olga', 'Ivan', 'Kirill', 'Vasya', 'Alexander', 'Anna', 'Polina']
+//     // const arrNames = ['Olga', 'Ivan', 'Kirill'];
 
-let [firstName, secondName, ...arrOtherName] = arrNames;
+// let [firstName, secondName, ...arrOtherName] = arrNames;
 
-for (let name of arrOtherName) {
-    if (arrOtherName.length < 2) {
-        secondName = 'alex'
-    } else {
-        console.log(name);
-    }
-}
-console.log(firstName);
-console.log(secondName);
+// for (let name of arrOtherName) {
+//     if (arrOtherName.length < 2) {
+//         secondName = 'alex'
+//     } else {
+//         console.log(name);
+//     }
+// }
+// console.log(firstName);
+// console.log(secondName);
 
 
 // // Задание 2
 // // Задать массив имен. Задать функцию, которая принимает 3 параметра и выводит их в консоль. Передать массив в функцию. Внутри функции каждый параметр должен быть равен одному из элементов массива.
 
-const arrNames = ['Olga', 'Ivan', 'Kirill', 'Vasya', 'Alexander', 'Anna', 'Polina'];
+// const arrNames = ['Olga', 'Ivan', 'Kirill', 'Vasya', 'Alexander', 'Anna', 'Polina'];
 
-function names1([firstName, secondName, thirdName]) {
-    console.log(firstName);
-    console.log(secondName);
-    console.log(thirdName);
-}
+// function names1([firstName, secondName, thirdName]) {
+//     console.log(firstName);
+//     console.log(secondName);
+//     console.log(thirdName);
+// }
 
-names1(arrNames)
+// names1(arrNames)
 
 
-function names2(arr) {
-    let [firstName, secondName, thirdName] = arr;
-    console.log(firstName);
-    console.log(secondName);
-    console.log(thirdName);
-}
+// function names2(arr) {
+//     let [firstName, secondName, thirdName] = arr;
+//     console.log(firstName);
+//     console.log(secondName);
+//     console.log(thirdName);
+// }
 
-names2(arrNames)
+// names2(arrNames)
 
 
 // Задание 3
 // Задать объект с N полей. Создать 2 переменные, которые будут называться так же, как поля, и хранить 2 значения полей объекта. Если соответствующих полей в объекте нет, задать значения чисел 1 и 2. Все остальные поля объекта записать в переменную fields.
 
 
+
 const person = {
-    name: 'Olga',
+    firstName: 'Olga',
     age: 29,
     address: {
         country: 'Belarus',
@@ -56,5 +57,11 @@ const person = {
     }
 }
 
-let { name, address: { city }, surName = 1, firstName = 2, ...fields } = person;
-console.log(name, city, surName, firstName, fields);
+let a = 'surName';
+let b = 'animal';
+person[a] = 1;
+person[b] = 2;
+
+let { firstName, address: { city }, surName, animal, ...fields } = person;
+
+console.log(firstName, city, surName, animal, fields);
